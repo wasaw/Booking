8782eadffcca70f3f710d78db6ed0043a0aa79f5
wasaw <merenkovalexander@gmail.com>
2023-12-19 11:00:55 +0300

    Init

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Александр Меренков on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
